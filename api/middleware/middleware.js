af064280ddc45328ac518a91b73eaf6b2b19059d
Willy Vega <willy.vegaa@Willys-MBP-2.lan>
2023-06-13 21:00:14 -0400

    created the validateUserId middleware

--- api/middleware/middleware.js
+++ api/middleware/middleware.js
@@ -1,17 +1,48 @@
+const User = require('../users/users-model');
+
 function logger(req, res, next) {
   // DO YOUR MAGIC
+  const timeStamp = new Date().toLocaleString();
+  const method = req.method;
+  const url = req.originalUrl;
+  console.log(`[${timeStamp}] ${method} to ${url}`);
+  next();
 }
 
-function validateUserId(req, res, next) {
-  // DO YOUR MAGIC
+async function validateUserId(req, res, next) {
+  try {
+    const user = await User.getById(req.params.id);
+    if (!user) {
+      res.status(404).json({
+        message: "user not found"
+      });
+    } else {
+      req.user = user;
+      next();
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: "Problem finding user"
+    });
+  }
 }
 
 function validateUser(req, res, next) {
   // DO YOUR MAGIC
+  console.log('validateUser middleware'); 
+  next();
 }
 
 function validatePost(req, res, next) {
   // DO YOUR MAGIC
+  console.log('validatePost middleware');
+  next();
 }
 
 // do not forget to expose these functions to other modules
+module.exports = {
+  logger,
+  validateUserId,
+  validateUser,
+  validatePost
+}
