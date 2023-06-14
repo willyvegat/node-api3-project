d24718353537d9d9d61ab9966f0b3909f9d097b3
Willy Vega <willy.vegaa@Willys-MBP-2.lan>
2023-06-13 21:28:46 -0400

    created validatePost middleware and used in post request for /:id/posts

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const {
   validateUserId,
-  validateUser
+  validateUser,
+  validatePost
 } = require('../middleware/middleware');
 
 // You will need `users-model.js` and `posts-model.js` both
@@ -45,11 +46,12 @@ router.get('/:id/posts', validateUserId, (req, res) => {
   console.log(req.user);
 });
 
-router.post('/:id/posts', validateUserId, (req, res) => {
+router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
   console.log(req.user);
+  console.log(req.text);
 });
 
 // do not forget to export the router
