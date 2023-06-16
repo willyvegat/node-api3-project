35b113c441df3de2765f741488883de44246a122
Willy Vega <willy.vegaa@Willys-MacBook-Pro-2.local>
2023-06-15 23:29:38 -0400

    delete request for user id working

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -50,10 +50,16 @@ router.put('/:id', validateUserId, validateUser, (req, res, next) => {
   .catch(next);
 });
 
-router.delete('/:id', validateUserId, (req, res) => {
+router.delete('/:id', validateUserId, async (req, res, next) => {
   // RETURN THE FRESHLY DELETED USER OBJECT
   // this needs a middleware to verify user id
-  console.log(req.user);
+  // console.log(req.user);
+  try {
+    await User.remove(req.params.id)
+    res.json(req.user);
+  } catch (err) {
+    next(err) 
+  }
 });
 
 router.get('/:id/posts', validateUserId, (req, res) => {
