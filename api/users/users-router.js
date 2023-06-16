580adb235306a117904e9b0aaf0bdfec2a919738
Willy Vega <willy.vegaa@Willys-MacBook-Pro-2.local>
2023-06-15 23:43:45 -0400

    creating new post by a specific id

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -74,12 +74,26 @@ router.get('/:id/posts', validateUserId, async (req, res, next) => {
   }
 });
 
-router.post('/:id/posts', validateUserId, validatePost, (req, res) => {
+router.post(
+  '/:id/posts', 
+  validateUserId, 
+  validatePost, 
+  async (req, res, next) => {
   // RETURN THE NEWLY CREATED USER POST
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
-  console.log(req.user);
-  console.log(req.text);
+  // console.log(req.user);
+  // console.log(req.text);
+    try {
+      // throw new Error('baaad!!');
+      const newPost = await Post.insert({
+        user_id: req.params.id,
+        text: req.text
+      });
+      res.status(201).json(newPost);
+    } catch (err) {
+      next(err);
+    }
 });
 
 router.use((err, req, res, next) => { // eslint-disable-line
