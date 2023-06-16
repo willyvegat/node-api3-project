3add64252a9796fb11fb751c3a693ff793d25fa3
Willy Vega <willy.vegaa@Willys-MacBook-Pro-2.local>
2023-06-15 21:18:13 -0400

    post endpoint creates new name with id

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -25,10 +25,16 @@ router.get('/:id', validateUserId, (req, res) => {
   res.json(req.user);
 });
 
-router.post('/', validateUser, (req, res) => {
+router.post('/', validateUser, (req, res, next) => {
   // RETURN THE NEWLY CREATED USER OBJECT
   // this needs a middleware to check that the request body is valid
-  console.log(req.name);
+  User.insert({ name: req.name })
+    .then(newUser => {
+      res.status(201).json(newUser);
+    })
+    .catch(err => {
+      next(err)
+    })
 });
 
 router.put('/:id', validateUserId, validateUser, (req, res) => {
